88f60dfb21006fc900b5d61fb08f3b2a836d6e59
pratheek017 <pratheek017@gmail.com>
2019-04-23 22:32:01 -0400

    Removed unwanted package imports.

--- app/src/main/java/fiix/challenge/fiixexercise/javasample/MainActivity2.java
+++ app/src/main/java/fiix/challenge/fiixexercise/javasample/MainActivity2.java
@@ -4,14 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import fiix.challenge.fiixexercise.R;
