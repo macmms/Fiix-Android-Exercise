079f6753e990f93a33e75e58ca857ec1dd9e7f7c
Petar Puric <puric.petarr@gmail.com>
2020-01-16 15:15:56 -0500

    Added DataProcessor and DataSource for retrieving answers

--- app/src/main/java/fiix/challenge/fiixexercise/javasample/MainActivity2.java
+++ app/src/main/java/fiix/challenge/fiixexercise/javasample/MainActivity2.java
@@ -8,11 +8,17 @@ import android.util.Log;
 import java.util.List;
 
 import fiix.challenge.fiixexercise.R;
+import fiix.challenge.fiixexercise.dp.DataProcessor;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private DataProcessor dp = new DataProcessor(new LocalDataSource());
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> answers = dp.getAnswers();
     }
 }
--- app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/MainActivity.kt
+++ app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/MainActivity.kt
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import fiix.challenge.fiixexercise.R
+import fiix.challenge.fiixexercise.dp.DataProcessor
 
 class MainActivity : AppCompatActivity() {
+
+    val dp = DataProcessor(LocalDataSource())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val answers = dp.getAnswers()
     }
 }
