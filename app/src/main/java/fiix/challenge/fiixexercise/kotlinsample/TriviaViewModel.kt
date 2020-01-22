177b31b16f476c014eebe586d11c8070e8967f13
Ivan Esparza <ivan-esparza@morsum.co>
2020-01-21 19:54:22 -0700

    Adds methods to trivia view model

--- app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/TriviaViewModel.kt
+++ app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/TriviaViewModel.kt
@@ -1,15 +1,26 @@
 package fiix.challenge.fiixexercise.kotlinsample
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import fiix.challenge.fiixexercise.dp.DataProcessor
-import java.lang.IllegalArgumentException
 
 class TriviaViewModel : ViewModel() {
     val dp = DataProcessor(LocalDataSource())
-    val answers = dp.getAnswers()
+    val repo = MockRepo()
+    var answers: List<String> = listOf()
 
+    private val questions = MutableLiveData<List<TriviaQuestion>>().apply {
+        postValue(repo.triviaQuestions)
+    }
 
-    fun getAnswer(questionIndex: Int): String {
-        return ""
+    private val loading = MutableLiveData<Boolean>()
+
+    fun getQuestions(): LiveData<List<TriviaQuestion>> = questions
+
+    fun getLoading(): LiveData<Boolean> = loading
+
+    fun fetchAnswer(questionIndex: Int) {
+
     }
 }
