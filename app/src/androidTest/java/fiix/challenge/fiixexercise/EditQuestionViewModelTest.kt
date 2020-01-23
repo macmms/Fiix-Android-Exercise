031dd99b40f035dd66ebf25891c3257928ab08bd
Ivan Esparza <ivan-esparza@morsum.co>
2020-01-23 00:55:40 -0700

    Adds test for edit question vm, changes ui

--- app/src/androidTest/java/fiix/challenge/fiixexercise/EditQuestionViewModelTest.kt
+++ app/src/androidTest/java/fiix/challenge/fiixexercise/EditQuestionViewModelTest.kt
@@ -7,13 +7,14 @@ import com.nhaarman.mockito_kotlin.*
 import fiix.challenge.fiixexercise.kotlinsample.data.TriviaQuestion
 import fiix.challenge.fiixexercise.kotlinsample.viewmodels.TriviaViewModel
 import fiix.challenge.fiixexercise.kotlinsample.data.TriviaRepository
+import fiix.challenge.fiixexercise.kotlinsample.viewmodels.EditQuestionViewModel
 import org.junit.*
 import org.junit.runner.*
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
 @RunWith(AndroidJUnit4::class)
-class TriviaViewModelTest {
+class EditQuestionViewModelTest {
 
     @Rule
     @JvmField
@@ -21,20 +22,20 @@ class TriviaViewModelTest {
 
     private val repository = mock(TriviaRepository::class.java)
 
-    private lateinit var viewModel: TriviaViewModel
+    private lateinit var viewModel: EditQuestionViewModel
 
     @Before
     fun setup() {
-        viewModel = TriviaViewModel(repository)
+        viewModel = EditQuestionViewModel(repository)
     }
 
     @Test
-    fun getAnswerSetsShowAnswerToTrue() {
-        val question = TriviaQuestion(0, "How are you?", "Good")
-        assert(!question.showAnswer)
+    fun saveQuestionSetsShowAnswerToFalse() {
+        val question = TriviaQuestion(0, "How are you?", "Good", true)
+        assert(question.showAnswer)
         val questionLiveData = MutableLiveData<TriviaQuestion>().apply { value = question }
         whenever(repository.getQuestion(any())).thenReturn(questionLiveData)
-        viewModel.getAnswer(question)
+        viewModel.saveQuestion(question)
         assert(question.showAnswer)
     }
 }
