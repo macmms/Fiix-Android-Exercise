62a0db54ad94d8e44ddd50c023260059cfba27f6
Ivan Esparza <ivan-esparza@morsum.co>
2020-01-22 23:19:49 -0700

    Shows question to edit and saves it to db

--- app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/ui/EditQuestionFragment.kt
+++ app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/ui/EditQuestionFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import fiix.challenge.fiixexercise.kotlinsample.data.TriviaQuestion
 import fiix.challenge.fiixexercise.kotlinsample.utils.InjectionUtils
 import fiix.challenge.fiixexercise.kotlinsample.viewmodels.EditQuestionViewModel
+import kotlinx.android.synthetic.main.fragment_edit_question.view.answerEditText
+import kotlinx.android.synthetic.main.fragment_edit_question.view.questionEditText
+import kotlinx.android.synthetic.main.fragment_edit_question.view.saveButton
 
 class EditQuestionFragment: Fragment() {
 
@@ -25,7 +31,26 @@ class EditQuestionFragment: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        check(questionId < 0) { "Invalid ID passed to EditQuestionFragment" }
+
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.getQuestion(questionId).observe(viewLifecycleOwner, Observer { question ->
+            view.apply {
+                questionEditText.setText(question.question, TextView.BufferType.EDITABLE)
+                answerEditText.setText(question.answer, TextView.BufferType.EDITABLE)
+                saveButton.setOnClickListener {
+                    val newQuestion = TriviaQuestion(
+                        questionId,
+                        questionEditText.text.toString(),
+                        answerEditText.text.toString()
+                    )
+                    viewModel.saveQuestion(newQuestion)
+                }
+            }
+        })
     }
 
     companion object {
