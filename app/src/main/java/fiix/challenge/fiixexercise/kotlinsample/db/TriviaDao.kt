560940604c870d9a1e3568b57f1953fa2651e24e
Alisha Sharma <alisha@Alishas-MacBook-Pro.local>
2020-03-04 21:57:28 -0500

    Added TriviaRepository and required DAO method

--- app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/db/TriviaDao.kt
+++ app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/db/TriviaDao.kt
@@ -9,7 +9,13 @@ abstract class TriviaDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insertTriviaItem(triviaItem: List<TriviaQuestion>)
 
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun updateTriviaItem(question: TriviaQuestion)
+
     @Query(value = "SELECT * FROM trivia")
     abstract fun getAll(): LiveData<List<TriviaQuestion>>
+
+    @Query(value = "SELECT * FROM trivia  WHERE id = :questionId")
+    abstract fun getTriviaQuestion(questionId: Int): TriviaQuestion
 
 }
---
+++ app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/repository/TriviaRepository.kt
@@ -0,0 +1,70 @@
+package fiix.challenge.fiixexercise.kotlinsample.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import fiix.challenge.fiixexercise.dp.DataProcessor
+import fiix.challenge.fiixexercise.kotlinsample.datasource.MockRepo
+import fiix.challenge.fiixexercise.kotlinsample.db.TriviaDAO
+import fiix.challenge.fiixexercise.kotlinsample.db.model.TriviaQuestion
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+
+class TriviaRepository(private val processor: DataProcessor, private val triviaDAO: TriviaDAO) {
+
+    companion object {
+        private var triviaRepository: TriviaRepository? = null
+
+        fun getInstance(processor: DataProcessor, triviaDAO: TriviaDAO): TriviaRepository? {
+            if (triviaRepository == null) {
+                triviaRepository = TriviaRepository(processor, triviaDAO)
+            }
+            return triviaRepository as TriviaRepository
+        }
+    }
+
+    private val questionsLive = MediatorLiveData<List<TriviaQuestion>>()
+
+    fun insertTrivia(triviaQuestions: List<TriviaQuestion>) {
+        insertAnswers(triviaQuestions)
+    }
+
+    fun getTrivia(): LiveData<List<TriviaQuestion>> {
+        val allQuestions = triviaDAO.getAll()
+        questionsLive.addSource(allQuestions) { questions ->
+            if (questions.isNullOrEmpty()) {
+                insertQuestions(MockRepo().triviaQuestions)
+            } else {
+                questionsLive.postValue(questions)
+            }
+        }
+        return questionsLive
+    }
+
+    fun getTriviaQuestion(questionId: Int): TriviaQuestion {
+        return runBlocking {
+            getTrivia(questionId).await()
+        }
+    }
+
+    private fun insertQuestions(triviaQuestion: List<TriviaQuestion>) = CoroutineScope(Dispatchers.IO).async {
+        triviaDAO.insertTriviaItem(triviaQuestion)
+
+    }
+
+    private fun insertAnswers(triviaQuestions: List<TriviaQuestion>) = CoroutineScope(Dispatchers.IO).async {
+        val answers = processor.getAnswers()
+        if (answers.size == triviaQuestions.size) {
+            answers.forEachIndexed { index, s ->
+                triviaQuestions[index].answer = s
+                triviaDAO.updateTriviaItem(triviaQuestions[index])
+            }
+        }
+    }
+
+    private fun getTrivia(questionId: Int) = CoroutineScope(Dispatchers.IO).async {
+        triviaDAO.getTriviaQuestion(questionId)
+    }
+
+}
