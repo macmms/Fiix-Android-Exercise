1988f90133c52930e99e2c5e7e2ef4edbf70708e
Raghav H Venkatesan <raghav.hv@gmail.com>
2021-09-18 09:40:25 -0400

    12: Set up action bar for back navigation

--- app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/view/MainActivity.kt
+++ app/src/main/java/fiix/challenge/fiixexercise/kotlinsample/view/MainActivity.kt
@@ -2,16 +2,37 @@ package fiix.challenge.fiixexercise.kotlinsample.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import fiix.challenge.fiixexercise.R
 import fiix.challenge.fiixexercise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Inflate layout through view binding available as a part of data binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setUpActionBar()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    private fun setUpActionBar() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
